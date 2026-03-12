461323f702525047fce1695353c26d76c3d52fe7
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2026-03-12 23:54:17 +0100

    boosts serialization spec

--- plugins/discourse-boosts/spec/serializers/post_serializer_spec.rb
+++ plugins/discourse-boosts/spec/serializers/post_serializer_spec.rb
@@ -19,6 +19,38 @@ RSpec.describe PostSerializer do
 
   before { SiteSetting.discourse_boosts_enabled = true }
 
+  describe "boosts" do
+    fab!(:boost) { Fabricate(:boost, post: target_post, user: user) }
+
+    it "includes serialized boosts" do
+      boosts = serialized[:boosts]
+
+      expect(boosts.length).to eq(1)
+      expect(boosts.first).to include(id: boost.id, cooked: boost.cooked)
+      expect(boosts.first[:user][:id]).to eq(user.id)
+    end
+
+    context "when plugin is disabled" do
+      before { SiteSetting.discourse_boosts_enabled = false }
+
+      it "excludes boosts" do
+        expect(serialized).not_to have_key(:boosts)
+      end
+    end
+
+    context "when boosts association is not loaded" do
+      let(:serialized) do
+        post = Post.find(target_post.id)
+        serializer = described_class.new(post, scope: guardian, root: false)
+        serializer.as_json
+      end
+
+      it "excludes boosts" do
+        expect(serialized).not_to have_key(:boosts)
+      end
+    end
+  end
+
   describe "can_boost" do
     it "is true for a regular user on another user's post" do
       expect(serialized[:can_boost]).to eq(true)
